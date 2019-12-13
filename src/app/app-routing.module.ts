e872655ff5d3a4634fc7dbaf4da1c6c5e0c9f929
yoddoe <yoddoe@gmail.com>
2019-12-14 04:28:42 +0700

    commit add html css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+// change component
+const routes: Routes = [
+  {path :"",redirectTo:"/login",pathMatch:"full"},
+  {path :"login:" component:LoginComponent},
+  {path :"register:" component:RegisterComponent},
+  {path :"home:" component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+//import library
+import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // add import
+    ReactiveFormsModule ,
+    FormsModule ,
+    HttpModule ,
+    HttpClientModule ,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
